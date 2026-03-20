6d40751c94a870bd1fc95cad952334daf24d39ad
bbtechpro <billy.barber@gmail.com>
2026-03-20 00:25:18 -0400

    Added condition to check for either first or last name missing

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
 function formatFullName(firstName, lastName) {
+    if (!firstName || !lastName) {
+        return "Invalid name input.";
+    }
 
     let fullName = `${lastName.charAt(0).toUpperCase() + lastName.slice(1)}, ${firstName.charAt(0).toUpperCase() + firstName.slice(1)}`;
 
     return fullName;
 }
 console.log(formatFullName("billy", "barber")); // Output: "Barber, Billy"
+
